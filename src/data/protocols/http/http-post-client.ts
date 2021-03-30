51cd70c1d431b2fa59ca9e1f31f7b102cdc02387
Athena Fernandes Sarantôpoulos <athenasaran@gmail.com>
2021-03-30 16:45:52 -0300

    feat: ensure remote authentication throw invalidaerror on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,8 +1,10 @@
+import { HttpResponse } from "./http-response";
+
 export type HttpPostParams = {
     url: string
     body?: object //opcional
 }
 
 export interface HttpPostClient{
-    post(params: HttpPostParams):Promise<void>
+    post(params: HttpPostParams):Promise<HttpResponse>
 }
--- src/data/usecases/authentication/remote-autentication.ts
+++ src/data/usecases/authentication/remote-autentication.ts
@@ -1,5 +1,7 @@
 import { AuthenticationParams } from "@/domain/usecases/autentication"
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
+import { InvalidCredentialsError } from "@/domain/erros/invalid-credentials-error";
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
 
 export class RemoteAuthentication{
     constructor(
@@ -10,10 +12,15 @@ export class RemoteAuthentication{
          }
 
     async auth(params: AuthenticationParams):Promise<void>{
-        await this.httpPostClient.post({
+       const httpResponse = await this.httpPostClient.post({
             url: this.url,
             body: params
         })
+
+        switch(httpResponse.statusCode) {
+            case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
+            default: return Promise.resolve()
+        }
        
     }
 }
